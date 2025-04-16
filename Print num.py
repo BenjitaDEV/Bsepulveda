d07a3703a4f0c99c276721331243af4ef7ba19a9
BenjitaDEV <benjamin.cespedes2708@gmail.com>
2025-04-16 16:40:43 -0400

    primer commit

---
+++ Print num.py
@@ -0,0 +1,32 @@
+# nombre="Deigo"
+# edad=69
+# print("Hello!!", nombre, "su edad es", edad)
+
+
+
+# #Solicitud de datos
+# print("ingrese su nombre")
+# nombre=input()
+# print("ingrese su edad")
+# edad=input()
+# #ejmplo de concadenacion
+# print("Hello!!", nombre, "su edad es", edad)
+
+# n1=int(input())
+# n2=int(input())
+
+# print("la suma de los numeros es ", n1+n2)
+
+print("ingresa 3 numeros")
+n1=int(input())
+n2=int(input())
+n3=int(input())
+prom=((n1+n2+n3)/3)
+print("el promedio es", prom )
+
+
+if prom>=4.0:
+    print("el alumno lamentablemente paso")
+else:
+    print("el alumno fracaso exitoasmente")
+
